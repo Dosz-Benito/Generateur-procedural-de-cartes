2906a41506008aa5917e2aa3d0bc5753d9d42752
Dosz-Benito <dosantosbenito81@gmail.com>
2026-08-17 01:40:30 +0100

    refactor: simplification des importations en supprimant la condition de package dans main.py

--- main.py
+++ main.py
@@ -4,20 +4,12 @@ from typing import Literal, NoReturn, Optional
 import pygame
 import sys
 
-if __package__:
-    from .scripts.generation import generer_carte
-    from .scripts.parametres.type import TypeDecoration, TypeEntite, TypeTuile
-    from .scripts.rendu import Rendu
-    from .scripts.utilitaires import debogage
-    from .scripts.parametres import DECALER_BAS, DECALER_DROITE, DECALER_GAUCHE, DECALER_HAUT, ENREGISTRER_CARTE, FPS, GENERER_CARTE, GENERER_CARTE_ILES, NOUVELLE_CARTE, OUVRIR_CARTE, TAILLE_AFFICHAGE, TAILLE_ECRAN, VITESSE_CAMERA
-    from .scripts.utilitaires.outils_images import charger_image, charger_images
-else:
-    from scripts.generation import generer_carte
-    from scripts.parametres.type import TypeDecoration, TypeEntite, TypeTuile
-    from scripts.rendu import Rendu
-    from scripts.utilitaires import debogage
-    from scripts.parametres import DECALER_BAS, DECALER_DROITE, DECALER_GAUCHE, DECALER_HAUT, ENREGISTRER_CARTE, FPS, GENERER_CARTE, GENERER_CARTE_ILES, NOUVELLE_CARTE, OUVRIR_CARTE, TAILLE_AFFICHAGE, TAILLE_ECRAN, VITESSE_CAMERA
-    from scripts.utilitaires.outils_images import charger_image, charger_images
+from scripts.generation import generer_carte
+from scripts.parametres.type import TypeDecoration, TypeEntite, TypeTuile
+from scripts.rendu import Rendu
+from scripts.utilitaires import debogage
+from scripts.parametres import DECALER_BAS, DECALER_DROITE, DECALER_GAUCHE, DECALER_HAUT, ENREGISTRER_CARTE, FPS, GENERER_CARTE, GENERER_CARTE_ILES, NOUVELLE_CARTE, OUVRIR_CARTE, TAILLE_AFFICHAGE, TAILLE_ECRAN, VITESSE_CAMERA
+from scripts.utilitaires.outils_images import charger_image, charger_images
 # Tester avec stubtest.exe
 
 
