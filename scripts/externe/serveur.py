f00757e2452b8aa78c2cfe152f7f1fa9ae603a81
Dosz-Benito <dosantosbenito81@gmail.com>
2026-08-21 15:21:26 +0100

    refactor: suppression de la route `accueil`

--- scripts/externe/serveur.py
+++ scripts/externe/serveur.py
@@ -2,24 +2,10 @@ import os
 os.environ["SDL_VIDEODRIVER"] = "dummy"
 from .. import preliminaires
 
-from fastapi import FastAPI, Response
+from fastapi import FastAPI
 from ..generation import generer_carte
 
 app = FastAPI()
-
-@app.get("/")
-def accueil():
-    return Response("""<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mon serveur de génération</title>
-</head>
-<body>
-    <h1>Bienvenue sur l'accueil du serveur Raydash !!!</h1>
-</body>
-</html>""")
 
 @app.get("/nouvelle_carte")
 def nouvelle_carte():
